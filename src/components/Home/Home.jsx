608fcfcac9005160007644c08489acd933b50c4b
Shakila46 <shakilapraween46@gmail.com>
2025-05-04 00:42:36 +0530

    feat: add in signup and login pages

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className ="text-4xl font-bold ">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default Home
